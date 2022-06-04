a84fdeab2eb561a9e6fd218be14923f86188700d
Amin_Seifi <aminseiifi@gmail.com>
2022-06-05 00:46:04 +0430

    feat(db): Change local db
    Change local db from sqlite3 to postgresql.

--- cryptor/settings/local.py
+++ cryptor/settings/local.py
@@ -4,8 +4,10 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cryptor_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres'
     }
 }
 
