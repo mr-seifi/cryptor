2096b0b472cc4935424ed0656a8b596a2f89dad3
Amin_Seifi <aminseiifi@gmail.com>
2022-06-06 03:08:30 +0430

    feat(strategy): Write calculate_target_shares
    Write target_shares which calculates the value for each target.

--- market/services.py
+++ market/services.py
@@ -3,10 +3,16 @@ from _helpers import singleton, DateTimeService
 from cryptor.settings import RELEASE_DATE
 from django.utils import timezone
 from django.db.models import Min, Sum
+import numpy as np
 
 
 @singleton
 class MarketService:
+
+    STRATEGY_SPLIT_PART = 3
+    STRATEGY_COEFFICIENT = {'low': np.array([1, 0, 0]),
+                            'medium': np.array([0.4, 0.6, 0]),
+                            'high': np.array([0.2, 0.3, 0.5])}
 
     @staticmethod
     def calculate_signals(start=RELEASE_DATE, end=timezone.now, queryset=None) -> int:
@@ -69,3 +75,20 @@ class MarketService:
         return cls.calculate_net(start=timezone.now() - timezone.timedelta(days=30),
                                  end=timezone.now(),
                                  queryset=queryset)
+
+    @staticmethod
+    def _strategy_validation(strategy: str, target_count: int) -> str:
+        if target_count >= 3 or strategy == 'low':
+            return strategy
+        if target_count < 2:
+            return 'low'
+        return 'medium'
+
+    @classmethod
+    def calculate_target_shares(cls, strategy: str, target_count: int):
+        assert target_count > 0, AssertionError('target count must be a positive number!')
+        strategy = cls._strategy_validation(strategy, target_count)
+        targets = np.array(np.array_split(np.ones(target_count), cls.STRATEGY_SPLIT_PART))
+        coefficient = (c := cls.STRATEGY_COEFFICIENT[strategy], c.reshape(3, 1))[targets.ndim == 2]
+        return np.concatenate(list(map(lambda x: x / x.shape[0], targets * coefficient))).tolist()
+
